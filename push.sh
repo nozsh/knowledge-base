e3eede5a407cc1e1ca83e21c68980696a8f670c5
nozsh <155148075+nozsh@users.noreply.github.com>
2024-05-14 14:38:44 +0300

    upd

--- push.sh
+++ push.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
 echo "- Purging public dir"
+echo
 rm -rf public/* && echo "- Purging public dir: Done"
 echo
-echo "Re-generate public dir"
+echo "- Re-generate public dir"
+echo
 hugo && echo "Re-generate public dir: Done"
 echo
-echo "Work with git"
+echo "- Work with git"
+echo
 git add . && git commit -m "upd" && git push
 echo
 echo "Done!"
